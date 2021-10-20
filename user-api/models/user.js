3c5236e7d3f2f94fba520ecc3962b0597c4132a7
Mitchell <arthur.154@osu.edu>
2021-10-19 19:17:19 -0700

    Foundation - Initial hard coded data working

---
+++ user-api/models/user.js
@@ -0,0 +1,14 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+//     id: Number,
+//     name: String,
+//     username: String,
+//     email: String,
+//     phone: String
+//     // address
+// });
+
+// // module.exports = mongoose.model('Author', authorSchema);
+// module.exports = userSchema;
