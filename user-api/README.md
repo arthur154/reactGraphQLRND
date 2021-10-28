0a4f9b28c4e10e67129e3507cff2fa4c71e003fb
Mitchell <arthur.154@osu.edu>
2021-10-27 17:55:23 -0700

    checkpoint (will change to CDK approach later)

--- user-api/README.md
+++ user-api/README.md
@@ -1,9 +1,8 @@
 To run this app locally: npm run dev
 Prerequisite: npm install -g nodemon
-https://www.npmjs.com/package/nodemon
+https://nodemon.io/
 
-
-To deploy this app to AWS:
+To deploy this app to AWS: serverless deploy --verbose
 Prerequisite: npm install -g serverless
-https://www.npmjs.com/package/serverless
+https://www.serverless.com/
 Configure serverless deployment user: serverless config credentials --provider aws --key {{aws_user_access_key}} --secret {{aws_user_secret}} -o
--- user-api/lambda.js
+++ user-api/lambda.js
@@ -1,13 +1,14 @@
 // lambda.js
 const { ApolloServer } = require("apollo-server-lambda");
-
 const { typeDefs, resolvers, mocks } = require("./graphql/schema");
+const { ApolloServerPluginInlineTrace } = require("apollo-server-core");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  mocks,
+  mocks: false,
   mockEntireSchema: false,
+  plugins: [ApolloServerPluginInlineTrace()],
   context: ({ event, context }) => ({
     headers: event.headers,
     functionName: context.functionName,
@@ -16,6 +17,8 @@ const server = new ApolloServer({
   })
 });
 
-module.exports = {
-  server
-};
+exports.graphqlHandler = server.createHandler();
+
+// module.exports = {
+//   server
+// };
